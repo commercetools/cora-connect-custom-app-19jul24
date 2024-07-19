4af86fd012697d67265ea9fb7ffa81c3599d503f
coradragutct <cora.dragut@commercetools.com>
2024-07-19 19:53:18 +0300

    added custom app code

---
+++ cora-custom-application-19jul/custom-application-config.mjs
@@ -0,0 +1,45 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Cora Custom App 19jul',
+  entryPointUriPath: '${env:ENTRY_POINT_URI_PATH}',
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
+  env: {
+    development: {
+      initialProjectKey: 'cora-test-13jun24',
+    },
+    production: {
+      applicationId: '${env:CUSTOM_APPLICATION_ID}',
+      url: '${env:APPLICATION_URL}',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products', 'view_shopping_lists'],
+    manage: ['manage_products', 'manage_shopping_lists'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+    {
+      uriPath: 'shopping-lists',
+      defaultLabel: 'Shopping Lists',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
